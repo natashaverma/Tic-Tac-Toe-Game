15c9dffbe6cef297730b03b1c2674d925c096d43
Mahima Chugh <34764608+mahima2112@users.noreply.github.com>
2020-02-27 18:04:10 +0530

    Update TicTacToeFrame.java

--- src/tictactoe/TicTacToeFrame.java
+++ src/tictactoe/TicTacToeFrame.java
@@ -29,7 +29,7 @@ public class TicTacToeFrame extends JFrame
     
     public TicTacToeFrame (){
         
-        JPanel  panel = new JPanel(new GridLayout(3, 3, 0, 0));
+        JPanel  panel = new JPanel(new GridLayout(3, 3, 0, 0)); // row,col,hgap,vgap
         
         for(int i =0 ;i < 3; i++)
              for(int j =0 ;j < 3; j++)
