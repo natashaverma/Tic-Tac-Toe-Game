c1a9ea7c91e8586a637e1f7da90d5f337c977d86
Mahima Chugh <34764608+mahima2112@users.noreply.github.com>
2020-02-27 18:17:30 +0530

    Update TicTacToe.java

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,31 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tictactoe;
+package ticTacToe;
 
-import javax.swing.JFrame;
-import java.awt.*;
-/**
- *
- * @author hp
- */
-public class TicTacToe {
+public class Board {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        JFrame ticTacToe = new TicTacToeFrame();
-        ticTacToe.setTitle("Tic Tac Toe Game");
-        ticTacToe.setSize(600,600);
-        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ticTacToe.setLocationRelativeTo(null);
-        ticTacToe.setVisible(true);
-        
-    }
-    
+	private char board[][];
+	private int boardSize = 3;
+	private int count;
+	private char p1Symbol;
+	private char p2Symbol;
+	public final static int PLAYER_1_WINS = 1;
+	public final static int PLAYER_2_WINS = 2;
+	public final static int DRAW = 3;
+	public final static int INCOMPLETE = 4;
+	public final static int INVALID = 5;
+
+	public Board(char p1Sym, char p2Sym) {
+		board = new char[boardSize][boardSize];
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				board[i][j] = ' ';
+			}
+		}
+		this.p1Symbol = p1Symbol;
+		this.p2Symbol = p2Symbol;
+	}
+
+	public int move(char symbol, int x, int y) {
+		if (x < 0 || x >= boardSize || y < 0 || y >= boardSize || board[x][y] != ' ') {
+			return INVALID;
+		}
+		board[x][y] = symbol;
+		count++;
+		// Check Row
+		if (board[x][0] == board[x][1] && board[x][0] == board[x][2]) {
+			return symbol == p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+		}
+		// Check Col
+		if (board[0][y] == board[1][y] && board[0][y] == board[2][y]) {
+			return symbol == p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+		}
+		// First Diagonal
+		if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+			return symbol == p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+		}
+		// Second Diagonal
+		if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+			return symbol == p1Symbol ? PLAYER_1_WINS : PLAYER_2_WINS;
+		}
+		if (count == boardSize * boardSize) {
+			return DRAW;
+		}
+		return INCOMPLETE;
+
+	}
+
+	public void print() {
+		System.out.println("---------------");
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				System.out.print("| " + board[i][j] + " |");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("---------------");
+	}
+
+	
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
